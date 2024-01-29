36036ce8839a6cd36e25a3918af185ab5fb0be08
J0EL-VR <158073791+J0EL-VR@users.noreply.github.com>
2024-01-29 21:05:35 +0000

    Separated char types for password.

--- password.py
+++ password.py
@@ -1,12 +1,27 @@
 import random
+from string import ascii_lowercase, ascii_uppercase, digits, punctuation
 
-posschar = "abcdefghijklmnopqrstuvwxyz1234567890-=!@#$%^&*()_+"
-ranchar = random.choice(posschar)
 print("Want a random password?")
-charcount = int(input("How many characters?: "))
-if charcount<1:
+countlower = int(input("How many lowercase characters?: "))
+countupper = int(input("How many uppercase characters?: "))
+countnum = int(input("How many numeral characters?: "))
+countsymb = int(input("How many special characters?: "))
+
+while countlower < 1 or countupper < 1 or countnum < 1 or countsymb < 1:
     print("Please choose an actual amount")
-    charcount = int(input("How many characters?: "))
-else:
-    for i in range(charcount):
-        print(ranchar)
+    countlower = int(input("How many lowercase characters?: "))
+    countupper = int(input("How many uppercase characters?: "))
+    countnum = int(input("How many numeral characters?: "))
+    countsymb = int(input("How many special characters?: "))
+
+password = ""
+for i in range(countlower):
+    password += random.choice(ascii_lowercase)
+for i in range(countupper):
+    password += random.choice(ascii_uppercase)
+for i in range(countnum):
+    password += random.choice(digits)
+for i in range(countsymb):
+    password += random.choice(punctuation)
+
+print(''.join(random.sample(password, len(password))))
